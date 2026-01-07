25d00cb505ba78cd37f11064c88b4e4d03877fc6
calebu <calebadeyemi@gmail.com>
2026-01-07 17:18:57 -0500

    Upload

---
+++ scripts/check.py
@@ -0,0 +1,22 @@
+import git
+import os
+import sys
+
+comments = sys.argv[1].splitlines()
+for comment_line in comments:
+  if 'source' in comment_line:
+    source = comment_line.split(':')
+    source[1] = source[1].strip()
+    try:
+      repo = git.Repo.clone_from(f'https://calebu:{sys.argv[2]}@github.com/calebu/' + source[1], source[1])
+      print(f"Repository successfully cloned to {os.path.abspath(source[1])}")
+    except Exception as clone_exc:
+      print(f"Unable to clone source repo: {clone_exc}")
+  elif 'target' in comment_line:
+    target = comment_line.split(':')
+    target[1] = target[1].strip()
+    try:
+      repo = git.Repo.clone_from(f'https://calebu:{sys.argv[2]}@github.com/calebu/' + target[1], target[1])
+      print(f"Repository successfully cloned to {os.path.abspath(target[1])}")
+    except Exception as clone_exc:
+      print(f"Unable to clone source repo: {clone_exc}")
